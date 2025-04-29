8350b7f314c1177df697635fa62d2c3ef4768392
Mike Wittenauer <mikewittenauer@me.com>
2025-04-29 11:24:09 -0400

    Fix bootstrap javascript issue

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,4 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails'// app/javascript/application.js
-//= require jquery3
-//= require popper
-//= require bootstrap
+// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+import "bootstrap"
